4e7bd51f35cf8ca9c99ee6291f42dd390395e4f9
umaretiya <umaretiya@gmail.com>
2022-05-19 00:25:31 +0530

    My first Social Site

---
+++ category/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.urls import reverse
+# Create your models here.
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=70, unique=True)
+    slug = models.SlugField(max_length=150, unique=True)
+    description = models.TextField(max_length=250, blank=True)
+    cat_image = models.ImageField(upload_to="photos/categories", blank=True)
+
+    class Meta:
+        verbose_name = "category"
+        verbose_name_plural = "categories"
+
+    def get_url(self):
+        return reverse("products_by_category", args=[self.slug])
+
+    def __str__(self):
+        return self.category_name
